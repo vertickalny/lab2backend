18f3c70b72483a2fc37e42803d9cc12bb94219b0
vertickalny <129714150+vertickalny@users.noreply.github.com>
2024-02-14 21:40:44 +0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,87 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const Task = require('./models/Task');
+
+const app = express();
+const PORT = 3500;
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true }).then(()=> console.log("Connected to MongoDB"))
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+
+// Create a task
+app.post('/tasks', async (req, res) => {
+    try {
+        const task = new Task(req.body);
+        await task.save();
+        res.status(201).send(task);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// Read all tasks
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find();
+        res.send(tasks);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// Read a specific task by ID
+app.get('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// Update a task by ID
+app.put('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['title', 'description', 'completed'];
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' });
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// Delete a task by ID
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
